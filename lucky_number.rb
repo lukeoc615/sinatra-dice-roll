5d22f8b32b42fafeb35c95a1e32fa3a0ff33d036
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-01 22:56:50 +0000

    created view template

--- lucky_number.rb
+++ lucky_number.rb
@@ -7,16 +7,8 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get ("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+get("/") do
+  erb(:elephant)
 end
 
 get ("/zebra") do
@@ -24,7 +16,7 @@ get ("/zebra") do
 end
 
 get ("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+  "Hopefully this shows up without having to restart the server "
 end
 
 get ("/dice/2/6") do
@@ -68,4 +60,3 @@ get ("/dice/5/4") do
   "<h1>5d4</h1>
   <p>#{outcome}</p>"
 end
-
